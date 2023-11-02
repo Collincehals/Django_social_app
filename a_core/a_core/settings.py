0c704b54c16db1ccc8be4b14c2251b43550dcb7e
Hals <odhiambocollince702@gmail.com>
2023-11-02 15:24:12 +0300

    whitenoise middleware adjusted

--- a_core/a_core/settings.py
+++ a_core/a_core/settings.py
@@ -57,6 +57,7 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -65,7 +66,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "allauth.account.middleware.AccountMiddleware",
     "django_htmx.middleware.HtmxMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 
