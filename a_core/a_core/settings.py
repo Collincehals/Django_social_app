e197c6063ec29772ce074058f5e808fc993d3120
Hals <odhiambocollince702@gmail.com>
2023-11-04 19:55:40 +0300

    whitenoise disabled

--- a_core/a_core/settings.py
+++ a_core/a_core/settings.py
@@ -24,7 +24,7 @@ import os
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['colltechgallery.onrender.com',
                  '127.0.0.1'
