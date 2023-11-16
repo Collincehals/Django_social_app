069ef06ae115f552d5274cb3542f7786f5c3df42
Hals <odhiambocollince702@gmail.com>
2023-11-16 19:48:13 +0300

    postgresql.db connected. Data migrated from sqlite

--- a_core/a_core/settings.py
+++ a_core/a_core/settings.py
@@ -124,19 +124,29 @@ SOCIALACCOUNT_PROVIDERS = {
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3', 
     }
 }
-
 """
 import os
 from dotenv import load_dotenv
 load_dotenv()
+DATABASES = {
+    'default': {
+        'ENGINE':'django.db.backends.postgresql',
+        'NAME':  os.environ.get('NAME'),  
+        'USER': os.environ.get('USER'),
+        'PASSWORD': os.environ.get('PASSWORD'), 
+        'HOST': os.environ.get('HOST'), 
+        'PORT': os.environ.get('PORT'), 
+    }
+}
 
+"""
 DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.mysql',
