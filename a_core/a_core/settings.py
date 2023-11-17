e212fa4599852981f07329b1206e3f2f9cd62d93
Hals <odhiambocollince702@gmail.com>
2023-11-17 07:59:12 +0300

    settings.py updated

--- a_core/a_core/settings.py
+++ a_core/a_core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import os
+
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -132,7 +132,6 @@ DATABASES = {
     }
 }
 """
-import os
 from dotenv import load_dotenv
 load_dotenv()
 DATABASES = {
